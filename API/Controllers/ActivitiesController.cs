7dbc335c387973246978955d80f8308f4824cebd
Noble010 <mubarakadesokan@gmail.com>
2025-07-24 17:05:53 +0100

    end of section 12

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -5,6 +5,7 @@ using Application.Activities.Queries;
 using Application.Core;
 using Domain;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
--- Persistence/DbInitializer.cs
+++ Persistence/DbInitializer.cs
@@ -1,12 +1,27 @@
 using System;
 using Domain;
+using Microsoft.AspNetCore.Identity;
 
 namespace Persistence;
 
 public class DbInitializer
 {
-    public static async Task SeedData(AppDbContext context)
+    public static async Task SeedData(AppDbContext context, UserManager<User>userManager)
     {
+        if (!userManager.Users.Any())
+        {
+            var users = new List<User>
+            {
+                new() {DisplayName = "Bob", UserName="bob@test.com", Email="bob@test.com"},
+                new() {DisplayName = "jane", UserName="jane@test.com", Email="jane@test.com"},
+                new() {DisplayName = "shai", UserName="shai@test.com", Email="shai@test.com"}
+            };
+            foreach (var user in users)
+            {
+                await userManager.CreateAsync(user, "Pa$$w0rd");
+            }
+        }
+
         if (context.Activities.Any()) return;
 
         var activities = new List<Activity>
